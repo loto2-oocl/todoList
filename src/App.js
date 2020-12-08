4f860979e7f5e78d2659febc10be7c9e57a09670
Tom Lo <loto2@oocl.com>
2020-12-09 00:22:43 +0800

    [todo][tom] feat: use antd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import TodoList from './components/TodoList';
+import { Button } from 'antd';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <TodoList />
+        <Button type="primary">Button</Button>
       </header>
     </div>
   );
