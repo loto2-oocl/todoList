fbc15604a1bd85ecc4d2e39093add17bee048caa
Tom Lo <loto2@oocl.com>
2020-12-09 20:56:14 +0800

    [todo][tom] refactor: todoItem use functional component

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,46 +1,45 @@
 import './TodoItem.css';
-import React, { Component } from 'react';
+import React from 'react';
 import { DONE } from '../constants/TodoItemStatus';
 import { Button, Col, Row, Space } from 'antd';
 import { CloseOutlined } from '@ant-design/icons';
 import { deleteTodoItem, toggleTodoStatus } from '../apis/todos';
 
-class TodoItem extends Component {
-  handleClick = () => {
-    const { id } = this.props.todoItem;
+const TodoItem = ({ todoItem, removeTodoItem, toggleChangeStatus }) => {
+  const { message, status } = todoItem;
+
+  const handleClick = () => {
+    const { id } = todoItem;
     deleteTodoItem(id).then(() => {
-      this.props.removeTodoItem(id);
+      removeTodoItem(id);
     });
   };
 
-  handleStatusChange = () => {
-    const { id } = this.props.todoItem;
-    toggleTodoStatus(this.props.todoItem).then(() => {
-      this.props.toggleChangeStatus(id);
+  const handleStatusChange = () => {
+    const { id } = todoItem;
+    toggleTodoStatus(todoItem).then(() => {
+      toggleChangeStatus(id);
     });
   };
 
-  render() {
-    const { message, status } = this.props.todoItem;
-    return (
-      <Row className="todo-item-row">
-        <Col flex="auto">
-          <span
-            className={status === DONE ? 'todo-item-done' : ''}
-            onClick={this.handleStatusChange}
-          >
-            {message}
-          </span>
-        </Col>
-        <Space />
-        <Col flex="50px">
-          <Button type="text" onClick={this.handleClick}>
-            <CloseOutlined className="todo-item-close-button" />
-          </Button>
-        </Col>
-      </Row>
-    );
-  }
-}
+  return (
+    <Row className="todo-item-row">
+      <Col flex="auto">
+        <span
+          className={status === DONE ? 'todo-item-done' : ''}
+          onClick={handleStatusChange}
+        >
+          {message}
+        </span>
+      </Col>
+      <Space />
+      <Col flex="50px">
+        <Button type="text" onClick={handleClick}>
+          <CloseOutlined className="todo-item-close-button" />
+        </Button>
+      </Col>
+    </Row>
+  );
+};
 
 export default TodoItem;
