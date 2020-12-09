b389fec4ad62537c1e0f7c7232f7331ff5968a2a
Tom Lo <loto2@oocl.com>
2020-12-09 22:09:40 +0800

    [todo][tom] feat: render tags in done list

--- src/components/DoneList.jsx
+++ src/components/DoneList.jsx
@@ -1,17 +1,28 @@
 import './DoneList.css';
-import { Card, List } from 'antd';
+import { Card, Col, List, Row, Tag } from 'antd';
 import React, { Component } from 'react';
 
 export default class DoneList extends Component {
   render() {
     const { doneList } = this.props;
 
+    const renderTags = (tags) => {
+      return tags.map((tag) => <Tag>{tag}</Tag>);
+    };
+
     return (
       <div className="donelist-card-wrapper">
         <Card title="Done List" bordered={false}>
           <List>
             {doneList.map((doneItem) => (
-              <List.Item key={doneItem.id}>{doneItem.message}</List.Item>
+              <List.Item key={doneItem.id}>
+                <Row gutter={[0, 10]}>
+                  <Col span={24}>
+                    <span>{doneItem.message}</span>
+                  </Col>
+                  <Col>{renderTags(doneItem.tags)}</Col>
+                </Row>
+              </List.Item>
             ))}
           </List>
         </Card>
