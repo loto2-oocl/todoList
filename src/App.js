1ae6cee6063e90004f91444ed3a195bb736d331a
Tom Lo <loto2@oocl.com>
2020-12-09 11:52:36 +0800

    [todo][tom] feat: add routing on app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
 import TodoList from './components/TodoList';
-import { Col, Row } from 'antd';
+import { Button, Col, Row } from 'antd';
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
+import DoneListContainer from './containers/DoneListContainer';
 
 function App() {
   return (
     <div className="App">
       <Row>
-        <Col flex="auto">
-          <TodoList />
+        <Col span={24}>
+          <BrowserRouter>
+            <Row>
+              <Col span="3">
+                <NavLink to="/">
+                  <Button block>Todo Page</Button>
+                </NavLink>
+              </Col>
+              <Col span="3">
+                <NavLink to="/done">
+                  <Button block>Done Page</Button>
+                </NavLink>
+              </Col>
+            </Row>
+            <Switch>
+              <Route path="/done" component={DoneListContainer} />
+              <Route exact path="/" component={TodoList} />
+            </Switch>
+          </BrowserRouter>
         </Col>
       </Row>
     </div>
